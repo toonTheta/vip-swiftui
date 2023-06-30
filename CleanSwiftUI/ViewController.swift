e6256282cb2b3f9ba1df1224487f6ee1d6f1a762
Siradanai <siradanai.s@lmwn.com>
2023-06-30 12:28:35 +0700

    Initial Commit

---
+++ CleanSwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSwiftUI
+//
+//  Created by Siradanai.s on 30/6/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
